35d9c73e86a03a4579cf62a62aba775628a8da08
torparawell <70603912+torparawell@users.noreply.github.com>
2023-11-06 17:57:55 +0000

    Add files via upload

--- cv.py
+++ cv.py
@@ -4,26 +4,32 @@ import cv2
 from pathlib import Path
 from PIL import Image
 import torch
+from torchvision import utils
 from torch.utils.data import Dataset, DataLoader, random_split
 from torchvision import transforms
 import torchvision
 from torchvision.models.detection import fasterrcnn_resnet50_fpn, FasterRCNN_ResNet50_FPN_Weights
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from tqdm import tqdm
+import argparse
 
-# If you use any other specific functions or classes from torchvision, import them here
-# For example, if you use specific transforms:
 from torchvision.transforms import functional as F
 
 # For the optimizer and scheduler
 from torch.optim import SGD
 from torch.optim.lr_scheduler import StepLR
 
+# Add command line arguments for training and model path
+parser = argparse.ArgumentParser(description="Object Detection Training")
+parser.add_argument("--train", action="store_true", help="Train the model")
+parser.add_argument("--model_path", type=str, default="model.pth", help="Path to save/load the model")
+args = parser.parse_args()
+
 class VideoDataset:
-    def __init__(self, base_path, annotations_path, videos_path, transform=None):
-        # Initialize dataset, you might want to add additional arguments to pass transforms etc.
+    def __init__(self, base_path, annotations_path, videos_path, scene='bookstore', video='video0', transform=None):
         self.base_path = Path(base_path)
-        self.annotations_path = self.base_path / annotations_path
-        self.videos_path = self.base_path / videos_path
+        self.annotations_path = self.base_path / annotations_path / scene / video
+        self.videos_path = self.base_path / videos_path / scene / video
         self.transform = transform
         self.videos_data = {}
         self.columns = [
@@ -34,25 +40,20 @@ class VideoDataset:
         self.frame_list = self.prepare_frame_list()
 
     def load_annotations(self):
-        for scene_dir in self.annotations_path.iterdir():
-            if scene_dir.is_dir():
-                for video_dir in scene_dir.iterdir():
-                    if video_dir.is_dir():
-                        annotation_file = video_dir / 'annotations.txt'
-                        if annotation_file.exists():
-                            data = pd.read_csv(annotation_file, sep=' ', header=None, names=self.columns)
-                            self.videos_data[(scene_dir.name, video_dir.name)] = data
-                        else:
-                            print(f"No annotation file found for video {video_dir.name} in scene {scene_dir.name}")
+        annotation_file = self.annotations_path / 'annotations.txt'
+        if annotation_file.exists():
+            data = pd.read_csv(annotation_file, sep=' ', header=None, names=self.columns)
+            self.videos_data = data
+        else:
+            print(f"No annotation file found for video in scene")
 
     def prepare_frame_list(self):
-        # Prepare a list of all frames and their corresponding annotations
         frame_list = []
-        for (scene, video), df in self.videos_data.items():
-            for frame_number in df['frame'].unique():
-                frame_annotations = df[df['frame'] == frame_number]
-                frame_list.append((scene, video, frame_number, frame_annotations))
+        for frame_number in self.videos_data['frame'].unique():
+            frame_annotations = self.videos_data[self.videos_data['frame'] == frame_number]
+            frame_list.append((frame_number, frame_annotations))
         return frame_list
+
     
     def __len__(self):
         # Return the total number of frames
@@ -60,8 +61,8 @@ class VideoDataset:
 
     def __getitem__(self, idx):
         # Retrieve the frame and annotations by index
-        scene, video, frame_number, annotations = self.frame_list[idx]
-        video_path = self.videos_path / scene / video / 'video.mov'
+        frame_number, annotations = self.frame_list[idx]
+        video_path = self.videos_path / 'video.mov'
         cap = cv2.VideoCapture(str(video_path))
         cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
         ret, frame = cap.read()
@@ -82,9 +83,22 @@ class VideoDataset:
         if self.transform:
             frame = self.transform(frame)
 
-        sample = {'image': frame, 'boxes': boxes, 'labels': labels}
+        # Initialize a padded set of boxes and labels
+        max_annotations = max(len(ann) for _, ann in self.frame_list)
+        padded_boxes = torch.zeros((max_annotations, 4), dtype=torch.float32)
+        padded_labels = torch.zeros((max_annotations,), dtype=torch.int64)
 
-        return sample
+        # Copy actual annotations into the padded tensors
+        num_annotations = len(annotations)
+        if num_annotations > 0:
+            padded_boxes[:num_annotations] = boxes
+            padded_labels[:num_annotations] = labels
+
+        # Create the target dictionary
+        target = {'boxes': padded_boxes, 'labels': padded_labels}
+
+        return frame, target  # Return a tuple with the image and the target dictionary
+
 
     
     def get_video_annotations(self, scene, video):
@@ -138,9 +152,8 @@ class ObjectDetectionModel:
     def get_transform(self):
         # Define the data transformations for the training and validation sets
         transform = transforms.Compose([
-            transforms.ToPILImage(),
-            transforms.Resize((800, 800)),
-            transforms.ToTensor(),
+            transforms.Resize((800, 800)), # This will work on a PIL Image
+            transforms.ToTensor(),         # This will convert the PIL Image to a Tensor
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
         return transform
@@ -151,10 +164,27 @@ class Trainer:
         self.optimizer = optimizer
         self.lr_scheduler = lr_scheduler
         self.device = device
+    
+    def train_one_batch(model, images, targets, optimizer):
+        images = list(image.to(device) for image in images)
+        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+        
+        loss_dict = model(images, targets)
+        losses = sum(loss for loss in loss_dict.values())
+
+        # Backpropagation
+        optimizer.zero_grad()
+        losses.backward()
+        optimizer.step()
+
+        return losses.item()
 
     def train_one_epoch(self, data_loader, epoch):
         self.model.train()
-        for images, targets in data_loader:
+        for i, (images, targets) in enumerate(data_loader):
+            if i == 0:  # Just to print the first batch
+                print(images)   # Should show a batch of images
+                print(targets)  # Should show corresponding batch of targets
             images = list(image.to(self.device) for image in images)
             targets = [{k: v.to(self.device) for k, v in t.items()} for t in targets]
             loss_dict = self.model(images, targets)
@@ -184,42 +214,82 @@ class Trainer:
             self.validate(val_loader)
             self.lr_scheduler.step()
 
-# In your main script
+# ... [the rest of your imports and class definitions]
+
 if __name__ == "__main__":
     base_path = 'stanford_campus_dataset'
-    num_classes = 5  # Including the background class
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
-    # Initialize the object detection model
+    num_classes = 7  # 6 classes + background
+    model_path = args.model_path
     object_detection_model = ObjectDetectionModel(num_classes)
-    model = object_detection_model.model
-    model.to(device)
 
-    # Create the dataset with transformations
-    dataset_transform = object_detection_model.get_transform()
-    dataset = VideoDataset(
-        base_path=base_path,
-        annotations_path='annotations',
-        videos_path='videos',
-        transform=dataset_transform
-    )
+    # Setup the device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    object_detection_model.model.to(device)
 
-    # Split the dataset into train and validation sets
-    train_size = int(0.8 * len(dataset))
-    val_size = len(dataset) - train_size
-    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+    # Check for a saved model
+    if os.path.exists(model_path) and not args.train:
+        print(f"Loading model from {model_path}")
+        object_detection_model.model.load_state_dict(torch.load(model_path, map_location=device))
+        object_detection_model.model.eval()  # if you're loading for inference/testing
+    else:
+        # Create the dataset with transformations
+        dataset = VideoDataset(
+            base_path=base_path,
+            annotations_path='annotations',
+            videos_path='videos',
+            scene='bookstore',
+            video='video0',
+            transform=object_detection_model.get_transform()
+        )
 
-    # Create data loaders
-    train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True, collate_fn=lambda x: x)
-    val_loader = DataLoader(val_dataset, batch_size=4, shuffle=False, collate_fn=lambda x: x)
+        # Split dataset into train and validation sets
+        train_size = int(0.8 * len(dataset))
+        val_size = len(dataset) - train_size
+        train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
 
-    # Initialize the optimizer and learning rate scheduler
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.005, momentum=0.9, weight_decay=0.0005)
-    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+        # Create the DataLoaders for our training and validation sets
+        train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
+        val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
 
-    # Initialize the Trainer
-    trainer = Trainer(model, optimizer, lr_scheduler, device)
+        # Define an optimizer
+        optimizer = SGD(object_detection_model.model.parameters(), lr=0.005, momentum=0.9, weight_decay=0.0005)
 
-    # Start training
-    num_epochs = 10
-    trainer.train(train_loader, val_loader, num_epochs)
+        # Define a learning rate scheduler
+        lr_scheduler = StepLR(optimizer, step_size=3, gamma=0.1)
+
+        # Trainer instance
+        trainer = Trainer(
+            model=object_detection_model.model,
+            optimizer=optimizer,
+            lr_scheduler=lr_scheduler,
+            device=device
+        )
+
+        # Training loop with tqdm progress bar
+        num_epochs = 10  # Define the number of epochs
+        for epoch in range(num_epochs):
+            with tqdm(total=len(train_loader), desc=f"Epoch {epoch+1}/{num_epochs}", unit='batch') as pbar:
+                for batch in train_loader:
+                    # Unpack the images and targets from the batch
+                    images, targets = batch
+
+                    # Move the images and targets to the device
+                    images = [image.to(device) for image in images]
+                    print(targets)
+                    targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+
+                    loss_dict = object_detection_model.model(images, targets)
+                    losses = sum(loss for loss in loss_dict.values())
+
+                    optimizer.zero_grad()
+                    losses.backward()
+                    optimizer.step()
+
+                    pbar.set_postfix({'loss': losses.item()})
+                    pbar.update(1)
+            
+            lr_scheduler.step()
+
+        # Save the model after training
+        torch.save(object_detection_model.model.state_dict(), model_path)
+        print(f"Model saved to {model_path}")
